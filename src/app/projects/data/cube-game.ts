c991dc3a8e8f0c391af7833d09c1c538e51abe7d
Jack Evans <jackevans24@googlemail.com>
2021-08-30 20:46:57 +0100

    Add youtube links to existing projects

--- src/app/projects/data/cube-game.ts
+++ src/app/projects/data/cube-game.ts
@@ -6,6 +6,7 @@ export const cubeGame: IProject = {
     description: 'A runner game where the player controls a cube through sets of obstacles',
     githubLink: 'https://github.com/JackEvans24/CubeGame',
     imageUrl: 'assets/images/cube-game.png',
+    youtubeId: 'o6cHEGJBa4g',
     // tslint:disable-next-line:no-bitwise
     tags: Tags.CSharp | Tags.Unity
 };
