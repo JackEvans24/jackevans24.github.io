2381e801aa108ad2731c4502b15aef89fe238782
Jack Evans <jackevans24@googlemail.com>
2021-08-31 22:16:33 +0100

    Remove the youtube modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectPreviewComponent } from './project-preview/project-preview.component';
 import { MaterialModule } from './material.module';
-import { YoutubeModalComponent } from './youtube-modal/youtube-modal.component';
 
 
 
 @NgModule({
   declarations: [
-    ProjectPreviewComponent,
-    YoutubeModalComponent
+    ProjectPreviewComponent
   ],
   imports: [
     CommonModule,
@@ -17,11 +15,7 @@ import { YoutubeModalComponent } from './youtube-modal/youtube-modal.component';
   ],
   exports: [
     ProjectPreviewComponent,
-    YoutubeModalComponent,
     MaterialModule
-  ],
-  entryComponents: [
-    YoutubeModalComponent
   ]
 })
 export class SharedModule { }
--- src/app/shared/youtube-modal/youtube-modal.data.ts
+++
@@ -1,3 +0,0 @@
-export interface YoutubeModalData {
-    youtubeEmbedId: string;
-}
