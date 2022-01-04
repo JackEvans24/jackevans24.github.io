82a339d3219b29f67fa89dfe2c546f7d8e598201
Jack Evans <jackevans24@googlemail.com>
2022-01-04 11:24:03 +0000

    Add fingerguns to games

--- src/app/games/services/games.service.ts
+++ src/app/games/services/games.service.ts
@@ -3,12 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { IGame } from 'src/app/models/game';
 
+import { fingerguns } from '../data/fingerguns';
 import { graveyardPrince } from '../data/graveyard-prince';
 import { scuttlebugDerby } from '../data/scuttlebug-derby';
 
 const games: IGame[] = [
     graveyardPrince,
-    scuttlebugDerby
+    scuttlebugDerby,
+    fingerguns
 ];
 
 @Injectable({
