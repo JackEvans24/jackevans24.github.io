d85677b04abf4fa582fe75636522f3f121033968
Jack Evans <jackevans24@googlemail.com>
2022-03-14 11:00:36 +0000

    Change game details to inline component

--- src/app/scoreboard/game-details/game-details.component.ts
+++ src/app/scoreboard/game-details/game-details.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
 
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { BoardGame } from '../models/board-game';
+import { UntilDestroy } from '@ngneat/until-destroy';
 import { Game } from '../models/scoreboard-game';
-import { Player } from '../models/scoreboard-player';
-import { ScoreboardService } from '../services/scoreboard.service';
 
 @UntilDestroy()
 @Component({
@@ -14,57 +10,14 @@ import { ScoreboardService } from '../services/scoreboard.service';
   styleUrls: ['./game-details.component.scss']
 })
 export class GameDetailsComponent implements OnInit {
-    public boardGameMap: any = {};
-    public playerMap: any = {};
-    public game?: Game;
-    public loading = true;
-    private gameId = 0;
+    @Input() public game?: Game;
+    public expanded = false;
 
-    constructor(private route: ActivatedRoute, private service: ScoreboardService) { }
+    constructor() { }
 
-    ngOnInit(): void {
-        this.getBoardGames();
+    ngOnInit(): void {}
 
-        this.route.params.pipe(untilDestroyed(this)).subscribe(params => {
-            this.gameId = +params.id;
-            this.updateGame();
-        });
-    }
-
-    private getBoardGames(): void {
-        this.service.getAllBoardGames().subscribe(boardGames => {
-            this.boardGameMap = boardGames.reduce((acc: any, curr: BoardGame) => {
-                acc[curr.id] = curr;
-                return acc;
-            }, {});
-        });
-    }
-
-    private updateGame(): void {
-        if (!this.gameId || isNaN(this.gameId) || this.gameId <= 0) {
-            return;
-        }
-
-        this.loading = true;
-
-        this.service.getGame(this.gameId)
-            .subscribe((game: Game) => {
-                this.game = game;
-                this.updatePlayerMap();
-                this.loading = false;
-            });
-    }
-
-    private updatePlayerMap(): void {
-        if (!this.game) {
-            return;
-        }
-
-        this.service.getPlayersForGame(this.game).subscribe(players => {
-            this.playerMap = players.reduce((acc: any, curr: Player) => {
-                acc[curr.id] = curr;
-                return acc;
-            }, {});
-        });
+    public toggleExpand(): void {
+        this.expanded = !this.expanded;
     }
 }
--- src/app/scoreboard/scoreboard-routing.module.ts
+++ src/app/scoreboard/scoreboard-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { BoardGamesComponent } from './board-games/board-games.component';
-import { GameDetailsComponent } from './game-details/game-details.component';
 
 import { PlayersComponent } from './players/players.component';
 
@@ -9,10 +8,6 @@ const routes: Route[] = [
     {
         path: 'games',
         component: BoardGamesComponent
-    },
-    {
-        path: 'games/:id',
-        component: GameDetailsComponent
     },
     {
         path: 'players',
