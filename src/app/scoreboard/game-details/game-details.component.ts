e05b64d41d0af59229d1261a85921a5cbb32719c
Jack Evans <jackevans24@googlemail.com>
2022-03-14 12:07:14 +0000

    Update components to use state

--- src/app/scoreboard/game-details/game-details.component.ts
+++ src/app/scoreboard/game-details/game-details.component.ts
@@ -1,21 +1,42 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { UntilDestroy } from '@ngneat/until-destroy';
-import { Game } from '../models/scoreboard-game';
+import { Store } from '@ngxs/store';
 
-@UntilDestroy()
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { BoardGame } from '../models/board-game';
+import { Game } from '../models/scoreboard-game';
+import { Player } from '../models/scoreboard-player';
+import { ScoreboardState } from '../store/scoreboard.state';
+
 @Component({
-  selector: 'app-game-details',
-  templateUrl: './game-details.component.html',
-  styleUrls: ['./game-details.component.scss']
+    selector: 'app-game-details',
+    templateUrl: './game-details.component.html',
+    styleUrls: ['./game-details.component.scss']
 })
+@UntilDestroy()
 export class GameDetailsComponent implements OnInit {
     @Input() public game?: Game;
+    public boardGamesMap: Record<number, BoardGame> = {};
+    public playersMap: Record<number, Player> = {};
     public expanded = false;
 
-    constructor() { }
+    constructor(private store: Store) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.subscribeToStoreData();
+    }
+
+    private subscribeToStoreData(): void {
+        this.store
+            .select(ScoreboardState.playersMap)
+            .pipe(untilDestroyed(this))
+            .subscribe(map => this.playersMap = map);
+
+        this.store
+            .select(ScoreboardState.boardGamesMap)
+            .pipe(untilDestroyed(this))
+            .subscribe(map => this.boardGamesMap = map);
+    }
 
     public toggleExpand(): void {
         this.expanded = !this.expanded;
