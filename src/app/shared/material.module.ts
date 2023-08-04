3659dac8467d79dedffa4abdbb7a5a2cf6c81053
Jack Evans <jackevans24@googlemail.com>
2023-08-04 14:21:22 +0100

    chore: add expansion module from material

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,30 +2,33 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-    imports: [
-        MatButtonModule,
-        MatCardModule,
-        MatDialogModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        MatTooltipModule
-      ],
-      exports: [
-        MatButtonModule,
-        MatCardModule,
-        MatDialogModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        MatTooltipModule
-      ],
-      declarations: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule
+  ],
+  declarations: [],
 })
 export class MaterialModule { }
