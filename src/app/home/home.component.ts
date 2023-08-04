c6818a83c7ed6fef20b403c0f10f1945f2afc460
Jack Evans <jackevans24@googlemail.com>
2023-08-04 14:24:29 +0100

    feat: add 'about me' and 'technical skills' to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+    public languages = [
+        'C#',
+        'JavaScript',
+        'TypeScript',
+        'SQL',
+        'HTML',
+        'CSS/SCSS',
+        'Python',
+        'HLSL',
+        'Bash'
+    ];
+
+    public frameworks = [
+        '.NET',
+        '.NET Core',
+        'Entity Framework',
+        'Node',
+        'Angular',
+        'React',
+        'JQuery',
+        'Git'
+    ];
+
+    public software = [
+        'Azure DevOps',
+        'Azure Portal',
+        'Firebase',
+        'Postman',
+        'IIS',
+        'XCode',
+        'SQL Server Management Studio'
+    ];
+
+    public gameDev = [
+        'Unity',
+        'Blender',
+        'ShaderLab',
+        'GIMP',
+        'Procreate',
+        'Audacity',
+        'Bfxr'
+    ];
+}
