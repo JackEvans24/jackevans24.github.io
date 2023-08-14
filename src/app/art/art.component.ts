055481548f3f4688a7fc90f459a90cac68fe715a
Jack Evans <jackevans24@googlemail.com>
2023-08-14 15:10:42 +0100

    feat: add handling for mobile resolutions

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IShader, shaders } from './data/shaders';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
@@ -14,18 +14,30 @@ const SHADER_URL_PARAMS = '?gui=false&paused=false&muted=false';
 export class ArtComponent implements OnInit {
     public shaders = shaders;
     public currentShaderUrl: SafeUrl | null = null;
+    public compactView = false;
 
     constructor(private route: ActivatedRoute, private sanitiser: DomSanitizer) { }
 
     ngOnInit(): void {
+        this.setCompactView();
+
         this.route.paramMap.subscribe(params => {
             const currentShader = shaders.find(shader => shader.url === params.get('shaderType'));
             this.currentShaderUrl = !!currentShader ? this.getSafeEmbedUrl(currentShader) : null;
         });
     }
 
-    public getSafeEmbedUrl(shader: IShader): SafeUrl {
+    private getSafeEmbedUrl(shader: IShader): SafeUrl {
         const url = SHADER_URL_ROOT + shader.embedUrl + SHADER_URL_PARAMS;
         return this.sanitiser.bypassSecurityTrustResourceUrl(url);
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(_: any): void {
+        this.setCompactView();
+    }
+
+    private setCompactView(): void {
+        this.compactView = window.innerWidth < 660;
     }
 }
