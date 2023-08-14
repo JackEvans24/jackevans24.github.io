269322a0bb99fec57feef39d9f4ed43afbd148f0
Jack Evans <jackevans24@googlemail.com>
2023-08-14 12:26:46 +0100

    feat: add cellular noise shader and switch to embedded shadertoy

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IShader, shaders } from './data/shaders';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+const SHADER_URL_ROOT = 'https://www.shadertoy.com/embed/';
+const SHADER_URL_PARAMS = '?gui=false&paused=false&muted=false';
 
 @Component({
     selector: 'app-art',
@@ -9,14 +13,19 @@ import { IShader, shaders } from './data/shaders';
 })
 export class ArtComponent implements OnInit {
     public shaders = shaders;
-    public currentShader?: IShader;
+    public currentShaderUrl: SafeUrl | null = null;
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute, private sanitiser: DomSanitizer) { }
 
     ngOnInit(): void {
         this.route.paramMap.subscribe(params => {
-            console.log(params);
-            this.currentShader = shaders.find(shader => shader.url === params.get('shaderType'));
+            const currentShader = shaders.find(shader => shader.url === params.get('shaderType'));
+            this.currentShaderUrl = !!currentShader ? this.getSafeEmbedUrl(currentShader) : null;
         });
+    }
+
+    public getSafeEmbedUrl(shader: IShader): SafeUrl {
+        const url = SHADER_URL_ROOT + shader.embedUrl + SHADER_URL_PARAMS;
+        return this.sanitiser.bypassSecurityTrustResourceUrl(url);
     }
 }
--- src/app/art/data/shaders.ts
+++ src/app/art/data/shaders.ts
@@ -1,9 +1,9 @@
 export interface IShader {
     name: string;
     url: string;
-    code: string;
+    embedUrl: string;
 }
 
 export const shaders: IShader[] = [
-    { name: 'Cellular Noise', url: 'cellular-noise', code: '<div>Hello world</div>' }
+    { name: 'Cellular Noise', url: 'cellular-noise', embedUrl: 'Dt2yWz' }
 ];
